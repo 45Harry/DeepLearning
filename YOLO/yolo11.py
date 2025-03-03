d9e85505378c57aa3482daaec6499d174c3de265
Harry <smartharry499@gmail.com>
2025-03-03 16:17:49 +0545

    tested

--- YOLO/yolo11.py
+++ YOLO/yolo11.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load a COCO-pretrained YOLO11n model
-model = YOLO("yolo11n.pt")
+model = YOLO("yolo12n.pt")
 
 # Train the model on the COCO8 example dataset for 100 epochs
 #results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
 
 # Run inference with the YOLO11n model on the 'bus.jpg' image
-results = model("path/to/bus.jpg",save = True,show=True)
+results = model("YOLO/data/13159549_3840_2160_30fps.mp4",save = True,show=True)
